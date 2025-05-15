08087d28a0b5b15b8b8e9b62df0184964980fd53
Melga71 <adriadresse71@gmail.com>
2025-05-15 11:35:49 +0200

    Update App.java

--- src/main/java/com/mycompany/apptelier/App.java
+++ src/main/java/com/mycompany/apptelier/App.java
@@ -153,6 +153,7 @@ public class App extends Application {
     
     public static void main(String[] args) {
         launch(args); 
-         FiabiliteMachine.calculerFiabiliteDepuisFichier("suiviMaintenance.txt");
+        FiabiliteMachine.calculerFiabiliteDepuisFichier("suiviMaintenance.txt");
+        DatabaseManager.creerTables();
     }
 }
